a76b7cf0858e91dad1b626039ffbf06460714106
Saima Arora <saimaarora82@gmail.com>
2026-03-16 19:35:03 +0530

    feat: intialize backend server with express setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express"); //loads express
+const cors = require("cors");
+require("dotenv").config();
+const app = express(); //initialize the server, middleware routes connect to app
+
+//Middleware
+app.use(cors()); //allow frontend to call backend, allows cross-origin requests
+app.use(express.json()); //allow server to read json from req
+
+//test routes
+app.get("/", (req, res)=>{
+    res.send("Api is running..");
+});
+const port = process.env.port || 5000;
+app.listen(port, ()=>{
+    console.log(`Server is running on port${port}`); //start the backend server, listen on port 5000 or env port
+});
