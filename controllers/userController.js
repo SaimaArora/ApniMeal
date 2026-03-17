acbd56dbf9053da71e1f9434f89514ffd1da3b84
Saima Arora <saimaarora82@gmail.com>
2026-03-17 20:27:49 +0530

    feat: implement login api with jwt authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
 
+//register new user
 const registerUser = async(req, res) => {
     try{
         const {name, email, password, role} = req.body; //extract user data, parses it using app.use(express.json()) middleware
@@ -36,6 +38,43 @@ const registerUser = async(req, res) => {
     }
 };
 
+//Login user
+const loginUser = async (req, res) =>{
+    try{
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }); //check if user exists
+        if(!user) {
+            return res.status(400).json({
+                message : "Invalid email or password"
+            });
+        }
+        const isMatch = await bcrypt.compare(password, user.password); //safely compare input plain text to hashsed db password
+        if(!isMatch) {
+            return res.status(400).json({
+                message: "Invalid Password or email"
+            });
+        }
+        //generate token
+        const token = generateToken(user._id); //create jwt using user id
+        res.json({
+            message:"Login successful",
+            token, //store token in frontend
+            user:{
+                _id:user._id,
+                name:user.name,
+                email:user.email,
+                role:user.role
+            }
+        });
+    }
+    catch(error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express")
-const { registerUser } = require("../controllers/userController");
+const { registerUser, loginUser } = require("../controllers/userController");
 
 const router = express.Router(); //create new router object, define routes on it, then export to use in server.js
+
 router.post("/register", registerUser); //when POST request is made to /api/users/register, call registerUser controller function
+router.post("/login", loginUser); //post /api/users/login
 module.exports = router; //export the router object to use in server.js
